4b82678e3f85a0dd10b9ec988e16d3ec328374de
Emulan <emulan.developer@gmail.com>
2019-05-01 13:26:41 +0200

    Fronted Service

--- index.php
+++ index.php
@@ -1,17 +1,304 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Pedro Moreno e Hijos Cocinas</title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- Favicons
+            ================================================== -->
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+        <!-- Stylesheet
+            ================================================== -->
+        <link rel="stylesheet" type="text/css"  href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/nivo-lightbox/nivo-lightbox.css">
+        <link rel="stylesheet" type="text/css" href="css/nivo-lightbox/default.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+              <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            <![endif]-->
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
+    <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+        <!-- Navigation
+            ==========================================-->
+        <nav id="menu" class="navbar navbar-default navbar-fixed-top">
+            <div class="container"> 
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                    <a class="navbar-brand page-scroll" href="#page-top"><img src="img/logo_complet.png" style="margin-top: -20px"></a> </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right" style="">
+                        <li><a href="#about" class="page-scroll">About</a></li>
+                        <li><a href="#services" class="page-scroll">Services</a></li>
+                        <li><a href="#portfolio" class="page-scroll">Projects</a></li>
+                        <li><a href="#team" class="page-scroll">Team</a></li>
+                        <li><a href="#contact" class="page-scroll">Contact</a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse --> 
+            </div>
+        </nav>
+        <!-- Header -->
+        <header id="header">
+            <div class="intro">
+                <div class="overlay">
+                    <div class="container">
+                        <div class="row">
+                            <div class="intro-text" style="float: left;">
+                                <img class="img-responsive center-block" src="img/logo_prin.png">
+                                <h1><img src="img/tituloLogo.png"></h1>
+                                <a href="#about" class="btn btn-custom btn-lg page-scroll">Proyectos</a> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About Section -->
+        <div id="about">
+            <div class="container">
+                <div class="section-title text-center center">
+                    <h2>Our Story</h2>
+                    <hr>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-md-6 text-center"> <img src="img/about.jpg" class="img-responsive" alt=""> </div>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="about-text">
+                            <h3>The Studio</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</p>
+                            <h3>How We Work</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Services Section -->
+        <div id="services">
+            <div class="container">
+                <div class="col-md-10 col-md-offset-1 section-title text-center">
+                    <h2>Our Services</h2>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4 service"><a href="views/viewsKitchen.php"> <img src="img/residential-design.jpg" class="img-responsive" alt="Project Title">
+                        <h3>Cocinas y Armários</h3></a>
+                        <p>Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque natoque etiam. Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend.</p>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 service"><a href="views/viewsElectricalAppliance.php"> <img src="img/office-design.jpg" class="img-responsive" alt="Project Title">
+                            <h3>Electrodomésticos</h3></a>
+                        <p>Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque natoque etiam. Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend.</p>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 service"><a href="https://aguarevitalizada.wordpress.com/"> <img src="img/granderfondo.jpg" class="img-responsive" alt="Project Title">
+                            <h3>Revitalización Agua Grander</h3></a>
+                        <p>Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend pellentesque natoque etiam. Lorem ipsum dolor sit amet placerat facilisis felis mi in tempus eleifend.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Portfolio Section -->
+        <div id="portfolio">
+            <div class="container">
+                <div class="section-title text-center center">
+                    <h2>Projects</h2>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+                </div>
+                <div class="categories">
+                    <ul class="cat">
+                        <li>
+                            <ol class="type">
+                                <li><a href="#" data-filter="*" class="active">All Projects</a></li>
+                                <li><a href="#" data-filter=".residential">Residential</a></li>
+                                <li><a href="#" data-filter=".office">Office</a></li>
+                                <li><a href="#" data-filter=".commercial">Commercial</a></li>
+                            </ol>
+                        </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="row">
+                    <div class="portfolio-items">
+                        <div class="col-sm-6 col-md-4 col-lg-4 residential">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/01-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/01-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 commercial">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/02-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/02-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 residential">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/03-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/03-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 residential">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/04-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/04-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 commercial">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/05-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/05-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 office">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/06-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/06-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 office">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/07-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/07-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 residential">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/08-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/08-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 commercial">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/09-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/09-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 office">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/10-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/10-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 office">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/11-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/11-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 col-md-4 col-lg-4 residential">
+                            <div class="portfolio-item">
+                                <div class="hover-bg"> <a href="img/portfolio/12-large.jpg" title="Project Title" data-lightbox-gallery="gallery1">
+                                        <div class="hover-text">
+                                            <h4>Project Name</h4>
+                                        </div>
+                                        <img src="img/portfolio/12-small.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact Section -->
+        <div id="contact">
+            <div class="container">
+                <div class="section-title text-center">
+                    <h2>Datos de contacto</h2>
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Contact Info</h3>
+                    <div class="contact-item"> <span>Dirección</span>
+                        <p>Calle Vespasiano, 1<br>Mérida (Badajoz)
+                            </p>
+                    </div>
+                    <div class="contact-item"> <span>Correo</span>
+                        <p>contacto</p>
+                    </div>
+                    <div class="contact-item"> <span>Teléfono</span>
+                        <p>telefono de contacto</p>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div style="width: 100%">
+                        <iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&height=600&hl=es&q=Calle%20Vespasiano%2C%201%2C%20M%C3%A9rida%2C%20Badajoz+(Moreno)&ie=UTF8&t=&z=18&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="footer">
+            <div class="container text-center">
+                <div class="social">
+                    <ul>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    </ul>
+                </div>
+                <div>
+                    <p>&copy; 2016 NEST. All rights reserved. Designed by <a href="http://www.templatewire.com" rel="nofollow">TemplateWire</a></p>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+        <script type="text/javascript" src="js/bootstrap.js"></script> 
+        <script type="text/javascript" src="js/SmoothScroll.js"></script> 
+        <script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+        <script type="text/javascript" src="js/jquery.isotope.js"></script> 
+        <script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+        <script type="text/javascript" src="js/contact_me.js"></script> 
+        <script type="text/javascript" src="js/main.js"></script>
     </body>
 </html>
